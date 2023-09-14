77587f2bc80642340a68ffec24534ab7438a7645
seagullz4 <96653729+seagullz4@users.noreply.github.com>
2023-09-14 22:55:38 +0800

    更新 hysteria2install.sh

--- hysteria2install.sh
+++ hysteria2install.sh
@@ -58,20 +58,20 @@ esac
 
 # 以下是默认安装操作，你可以在这里添加安装代码
 
-# 创建hy3文件夹并进入
+# 创建 hy3 文件夹并进入
 mkdir -p ~/hy3
 cd ~/hy3
 
-# 下载Hysteria二进制文件并授予最高权限
+# 下载 Hysteria 二进制文件并授予最高权限
 wget -O hysteria-linux-amd64-avx https://github.com/apernet/hysteria/releases/download/app/v2.0.1/hysteria-linux-amd64-avx
 chmod +x hysteria-linux-amd64-avx
 
 # 获取当前用户名
 current_user=$(whoami)
 
-# 创建config.yaml文件并写入默认内容
+# 创建 config.yaml 文件并写入默认内容
 cat <<EOL > config.yaml
-# listen: :443
+listen: :443
 
 acme:
   domains:
@@ -88,6 +88,27 @@ masquerade:
     url: https://news.ycombinator.com/
     rewriteHost: true
 EOL
+
+# 用户输入端口号
+echo "$(random_color '请输入端口号（留空默认443，输入0随机2000-60000，输入1-65630为指定端口号）: ')"
+read -p "" port
+
+# 如果端口号为空，则默认为443
+if [ -z "$port" ]; then
+  port=443
+elif [ "$port" -eq "0" ]; then
+  # 如果端口号为0，则随机生成2000-60000之间的端口号
+  port=$((RANDOM % 58001 + 2000))
+elif [ "$port" -ge 1 ] && [ "$port" -le 65630 ]; then
+  # 如果端口号在1-65630之间，则使用用户输入的端口号
+  :
+else
+  echo "无效的端口号，退出脚本。"
+  exit
+fi
+
+# 替换配置文件中的端口号
+sed -i "s/443/$port/" config.yaml
 
 # 提示用户输入域名
 echo "$(random_color '请输入你的域名(必须是解析好的域名哦)（your.domain.net）: ')"
@@ -138,17 +159,16 @@ fi
 # 替换配置文件中的伪装域名
 sed -i "s|https://news.ycombinator.com/|$masquerade_url|" config.yaml
 
-# 授予Hysteria二进制文件权限
+# 授予 Hysteria 二进制文件权限
 sudo setcap cap_net_bind_service=+ep hysteria-linux-amd64-avx
 
-# 后台运行Hysteria服务器
+# 后台运行 Hysteria 服务器
 nohup ./hysteria-linux-amd64-avx server &
-
 
 line_animation
 
-# 输出Hysteria链接
-echo -e "$(random_color '这是你的Hysteria2节点链接，请注意保存哦宝宝: ')hy2://$password@$domain:443/?sni=$domain#Hysteria2"
+# 输出 Hysteria 链接
+echo -e "$(random_color '这是你的Hysteria2节点链接，请注意保存哦宝宝: ')hy2://$password@$domain:$port/?sni=$domain#Hysteria2"
 
 # 输出安装成功信息
 echo -e "$(random_color 'Hysteria2安装成功，请合理使用哦。')"
