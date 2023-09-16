8a6ee2b5172502962e01ec3a50b5f451a6ceadf7
zzxbaibai <96653729+seagullz4@users.noreply.github.com>
2023-09-16 22:21:48 +0800

    更新 hy2amd.sh

--- hy2amd.sh
+++ hy2amd.sh
@@ -272,3 +272,32 @@ echo -e "$(random_color '
 echo -e "$(random_color '
 
 Hysteria2安装成功，请合理使用哦。')"
+
+# Create a systemd service for Hysteria Server
+
+# Define service unit file path
+SERVICE_FILE="/etc/systemd/system/hysteria.service"
+
+# Define your command and working directory
+COMMAND="cd /root/hy3/ && nohup ./hysteria-linux-amd64 server"
+WORKING_DIR="/root/hy3/"
+
+# Create the service unit file
+cat > $SERVICE_FILE <<EOL
+[Unit]
+Description=Hysteria Server
+
+[Service]
+ExecStart=$COMMAND
+WorkingDirectory=$WORKING_DIR
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+# Enable and start the service
+systemctl enable hysteria.service
+systemctl start hysteria.service
+
+echo "Hysteria Server systemd service has been created and started."
