498ae06238f68bac564cf3cd5fde64673a879b8f
zzxbaibai <96653729+seagullz4@users.noreply.github.com>
2023-09-17 11:59:02 +0800

    更新 install.sh

--- install.sh
+++ install.sh
@@ -7,9 +7,6 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-# 如果是 root 用户，继续执行下面的命令
-echo "好的，你可以执行了"
-
 # 在这里添加你的其他命令
 
 # Define commands to install
@@ -20,19 +17,13 @@ install_missing_commands() {
   for cmd in "${commands[@]}"; do
     if ! command -v "$cmd" &>/dev/null; then
       echo "Installing $cmd..."
-      if [ "$(id -u)" -ne 0 ]; then
-        sudo_cmd="sudo"
-      else
-        sudo_cmd=""
-      fi
-
       if [ -x "$(command -v apt)" ]; then
         # Ubuntu/Debian
-        $sudo_cmd apt update
-        $sudo_cmd apt install -y "$cmd"
+        sudo apt update
+        sudo apt install -y "$cmd"
       elif [ -x "$(command -v yum)" ]; then
         # CentOS/RHEL
-        $sudo_cmd yum install -y "$cmd"
+        sudo yum install -y "$cmd"
       else
         echo "Unsupported package manager."
         exit 1
@@ -58,20 +49,24 @@ echo "Command installation complete."
 echo "正在识别系统架构中，请稍候……"
 arch=$(uname -m)
 
-if [ "$arch" == "x86_64" ] || [ "$arch" == "amd64" ]; then
-  echo "识别成功！为 x86_64/amd64 架构，正在运行对应命令……"
-  script_url="https://github.com/seagullz4/hysteria2/raw/main/hy2amd.sh"
-elif [ "$arch" == "aarch64" ]; then
-  echo "识别成功！为 arm64 架构，正在运行对应命令……"
-  script_url="https://github.com/seagullz4/hysteria2/raw/main/hy2arm.sh"
-else
-  echo "不支持的架构: $arch"
-  exit 1
-fi
+case $arch in
+  x86_64|amd64)
+    echo "识别成功！为 x86_64/amd64 架构，正在运行对应命令……"
+    script_url="https://github.com/seagullz4/hysteria2/raw/main/hy2amd.sh"
+    ;;
+  aarch64)
+    echo "识别成功！为 arm64 架构，正在运行对应命令……"
+    script_url="https://github.com/seagullz4/hysteria2/raw/main/hy2arm.sh"
+    ;;
+  *)
+    echo "不支持的架构: $arch"
+    exit 1
+    ;;
+esac
 
 # Download the script
 if wget -O hysteria2-install-script.sh "$script_url"; then
-  chmod +x hysteria2-install-script.sh
+  chmod +x hysteria2-install-script.sh  # 授予下载的脚本执行权限
   echo "下载并授予脚本执行权限成功。"
 else
   echo "下载脚本失败。退出。"
@@ -79,7 +74,7 @@ else
 fi
 
 # Execute the downloaded script with elevated privileges
-if sudo chmod +777 hysteria2-install-script.sh && sudo ./hysteria2-install-script.sh; then
+if sudo ./hysteria2-install-script.sh; then
   echo "脚本执行成功。"
 else
   echo "脚本执行失败。"
