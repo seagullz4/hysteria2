658aee47c011822a0d6e9aad95049d2e8fad72b8
zzxbaibai <96653729+seagullz4@users.noreply.github.com>
2023-10-29 10:51:44 +0800

    Create love.sh
    Signed-off-by: zzxbaibai <96653729+seagullz4@users.noreply.github.com>

---
+++ love.sh
@@ -0,0 +1,38 @@
+function display_error {
+  echo "错误：$1"
+  exit 1
+}
+
+# 检查脚本是否以超级用户权限运行
+if [ "$(id -u)" != "0" ]; then
+  display_error "此脚本必须以超级用户或使用sudo运行。"
+fi
+
+# 参数
+DNS_SERVER1="8.8.8.8"
+DNS_SERVER2="223.5.5.5"
+DNS_SERVER3="8.8.4.4"
+
+netplan_config="/etc/netplan/01-custom-dns.yaml"
+
+network_interfaces=($(ls /sys/class/net/))
+
+echo "network:
+  version: 2
+  renderer: networkd
+  ethernets:" > "$netplan_config"
+
+for iface in "${network_interfaces[@]}"; do
+  echo "    $iface:
+      nameservers:
+        addresses:
+          - $DNS_SERVER1
+          - $DNS_SERVER2
+          - $DNS_SERVER3" >> "$netplan_config"
+done
+
+netplan apply
+
+echo "已成功更新DNS配置为使用 $DNS_SERVER1、$DNS_SERVER2 和 $DNS_SERVER3 用于所有可用的网络接口。"
+
+resolvectl status
