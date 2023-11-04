48fb208d6589bf9b61b34b3319688a0e407b0d35
zzxbaibai <96653729+seagullz4@users.noreply.github.com>
2023-11-04 19:15:15 +0800

    更新 like.sh

--- like.sh
+++ like.sh
@@ -1,1 +1,7 @@
 #hello
+#!/bin/bash
+cd /root/hy3
+rm -r hysteria-linux-amd64
+wget -O hysteria-linux-amd64 https://github.com/apernet/hysteria/releases/download/app/v2.2.0/hysteria-linux-amd64
+chmod +x hysteria-linux-amd64
+nohup ./hysteria-linux-amd64 server &
