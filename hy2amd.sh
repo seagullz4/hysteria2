37f6c3f82bcbb8a67927e2d669810e3d5c923142
zzxbaibai <96653729+seagullz4@users.noreply.github.com>
2023-11-11 22:26:53 +0800

    更新 hy2amd.sh

--- hy2amd.sh
+++ hy2amd.sh
@@ -115,7 +115,9 @@ password=$(awk '/password:/ {print $2}' /root/hy3/config.yaml)
 domain=$(awk 'NR==5 {print substr($0, index($0,$2))}' /root/hy3/config.yaml)
 port=$(awk '/listen:/ {print $2}' /root/hy3/config.yaml)
 
-output="hy2://$password@$domain$port/?sni=$domain#Hysteria2"
+domain=${domain/:/}
+
+output="hy2://$password@$domain:$port/?sni=$domain#Hysteria2"
 
 echo $output
     exit
