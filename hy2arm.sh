3bf047e9ba69798b9f69e36590048580fdf4ef8c
zzxbaibai <96653729+seagullz4@users.noreply.github.com>
2023-11-22 16:09:36 +0800

    Update hy2arm.sh
    Signed-off-by: zzxbaibai <96653729+seagullz4@users.noreply.github.com>

--- hy2arm.sh
+++ hy2arm.sh
@@ -15,9 +15,6 @@ line_animation() {
     lines=$((lines + 1))
   done
 }
-#小火车开起来
-clear
-sl
 
 #这个y也是给用户看动画的
 welcome() {
@@ -46,7 +43,7 @@ echo "$(random_color '>>>>>>>>>>>>>>>>>>>>')"
 echo "3. 查看配置(穿越时空)"
 echo "4. 退出脚本(回到未来)"
 echo "$(random_color '>>>>>>>>>>>>>>>>>>>>')"
-echo "5. 在线更新hy2内核(目前版本2.2.0)"
+echo "5. 在线更新hy2内核(目前版本2.2.1)"
 echo "$(random_color 'hy2究极版本v23.11.11')"
 
 read -p "输入操作编号 (1/2/3/4/5): " choice
@@ -157,7 +154,7 @@ cd /root/hy3
 
 rm -r hysteria-linux-arm64
 
-wget -O hysteria-linux-arm64 https://github.com/apernet/hysteria/releases/download/app/v2.2.0/hysteria-linux-arm64
+wget -O hysteria-linux-arm64 https://github.com/apernet/hysteria/releases/download/app/v2.2.1/hysteria-linux-arm64
 
 chmod +x hysteria-linux-arm64
 
@@ -182,7 +179,7 @@ mkdir -p ~/hy3
 cd ~/hy3
 
 # Download the Hysteria binary and grant highest permissions
-if wget -O hysteria-linux-arm64 https://github.com/apernet/hysteria/releases/download/app/v2.2.0/hysteria-linux-arm64; then
+if wget -O hysteria-linux-arm64 https://github.com/apernet/hysteria/releases/download/app/v2.2.1/hysteria-linux-arm64; then
   chmod +x hysteria-linux-arm64
 else
   echo "$(random_color '下载 Hysteria 二进制文件失败，退出脚本。')"
