9f249ea07ffa5ea3dafa28c7125d201cc0ff3f40
zzxbaibai <96653729+seagullz4@users.noreply.github.com>
2023-12-03 10:58:51 +0800

    更新 hy2amd.sh

--- hy2amd.sh
+++ hy2amd.sh
@@ -45,7 +45,7 @@ echo "3. 查看配置(穿越时空)"
 echo "4. 退出脚本(回到未来)"
 echo "$(random_color '>>>>>>>>>>>>>>>>>>>>')"
 echo "5. 在线更新hy2内核(目前版本2.2.2)"
-echo "$(random_color 'hy2究极版本v23.11.11')"
+echo "$(random_color 'hy2究极版本v23.12.03')"
 
 read -p "输入操作编号 (1/2/3/4/5): " choice
 
