2c8cb901f97172aa5bd0f64fd8be8f4df9a75bad
seagullz4 <96653729+seagullz4@users.noreply.github.com>
2023-12-12 22:58:33 +0800

    更新 install.sh

--- install.sh
+++ install.sh
@@ -121,7 +121,7 @@ echo "3. 查看配置(穿越时空)"
 echo "4. 退出脚本(回到未来)"
 echo "$(random_color '>>>>>>>>>>>>>>>>>>>>')"
 echo "5. 在线更新hy2内核(目前版本2.2.2)"
-echo "$(random_color 'hy2究极版本v23.12.03')"
+echo "$(random_color 'hy2究极版本v23.12.12')"
 echo "$(random_color '>>>>>>>>>>>>>>>>>>>>')"
 echo "hysteria2状态: $hy2zt"
 
