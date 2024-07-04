f80754d2d3690e3aa81ef4bc8bb7459051da6544
0x648 <98635300+0x648@users.noreply.github.com>
2024-07-04 08:39:35 +0800

    Update install.sh
    Signed-off-by: 0x648 <98635300+0x648@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -650,6 +650,7 @@ generate_certificate() {
     read -p "请输入要用于自签名证书的域名（默认为 bing.com）: " user_domain
     domain_name=${user_domain:-"bing.com"}
     if curl --output /dev/null --silent --head --fail "$domain_name"; then
+        mkdir -p /etc/ssl/private
         openssl req -x509 -nodes -newkey ec:<(openssl ecparam -name prime256v1) -keyout "/etc/ssl/private/$domain_name.key" -out "/etc/ssl/private/$domain_name.crt" -subj "/CN=$domain_name" -days 36500
         chmod 777 "/etc/ssl/private/$domain_name.key" "/etc/ssl/private/$domain_name.crt"
         echo -e "自签名证书和私钥已生成！"
